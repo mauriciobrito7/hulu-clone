a29e907d309987c0308256332587afab2ce27963
Mauricio <maubot01@gmail.com>
2021-02-21 01:01:34 -0400

    global styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Results from "./components/Results/Results";
+import moviesRequests from "./utils/requests";
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState(
+    moviesRequests.fecthTrending
+  );
+
   return (
     <div className="app">
       <Header />
-      <Nav />
-      <Results />
+      <Nav setSelectedOption={setSelectedOption} />
+      <Results selectedOption={selectedOption} />
     </div>
   );
 }

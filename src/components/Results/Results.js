0563b32414b9068e0b4065774579fdc416487b48
Mauricio <maubot01@gmail.com>
2021-02-21 01:02:40 -0400

    animation on each card

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,26 +1,28 @@
 import React, { useState, useEffect } from "react";
 import VideoCard from "../VideoCard/VideoCard";
 import "./Results.css";
-import moviesRequests from "../../utils/requests";
 import axios from "../../utils/axios";
+import FlipMove from "react-flip-move";
 
-const Results = () => {
+const Results = ({ selectedOption }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(moviesRequests.fecthTrending);
+      const request = await axios.get(selectedOption);
       setMovies(request.data.results);
       return request;
     }
     fetchData();
-  }, []);
+  }, [selectedOption]);
 
   return (
     <div className="results">
-      {movies.map((movie) => (
-        <VideoCard key={movie.id} movie={movie} />
-      ))}
+      <FlipMove>
+        {movies.map((movie) => (
+          <VideoCard key={movie.id} movie={movie} />
+        ))}
+      </FlipMove>
     </div>
   );
 };

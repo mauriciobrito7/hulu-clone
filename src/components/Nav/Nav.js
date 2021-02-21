913580c0b052820b3cddbbe7e2e3d8452b796972
Mauricio <maubot01@gmail.com>
2021-02-21 01:02:05 -0400

    Nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,19 +1,83 @@
 import React from "react";
+import moviesRequests from "../../utils/requests";
 import "./Nav.css";
 
-const Nav = () => {
+const Nav = ({ setSelectedOption }) => {
+  const handleOption = (option) => {
+    setSelectedOption(option);
+  };
   return (
     <div className="nav">
-      <h2>Trending</h2>
-      <h2>Top rated</h2>
-      <h2>Action</h2>
-      <h2>Comedy</h2>
-      <h2>Horror</h2>
-      <h2>Mystery</h2>
-      <h2>Sci-Fi</h2>
-      <h2>Western</h2>
-      <h2>Animation</h2>
-      <h2>Movie</h2>
+      <h3
+        onClick={() => {
+          handleOption(moviesRequests.fecthTrending);
+        }}
+      >
+        Trending
+      </h3>
+      <h3
+        onClick={() => {
+          handleOption(moviesRequests.fetchTopRated);
+        }}
+      >
+        Top rated
+      </h3>
+      <h3
+        onClick={() => {
+          handleOption(moviesRequests.fecthActionMovies);
+        }}
+      >
+        Action
+      </h3>
+      <h3
+        onClick={() => {
+          handleOption(moviesRequests.fetchComedyMovies);
+        }}
+      >
+        Comedy
+      </h3>
+      <h3
+        onClick={() => {
+          handleOption(moviesRequests.fetchHorrorMovies);
+        }}
+      >
+        Horror
+      </h3>
+      <h3
+        onClick={() => {
+          handleOption(moviesRequests.fetchMystery);
+        }}
+      >
+        Mystery
+      </h3>
+      <h3
+        onClick={() => {
+          handleOption(moviesRequests.fetchSciFi);
+        }}
+      >
+        Sci-Fi
+      </h3>
+      <h3
+        onClick={() => {
+          handleOption(moviesRequests.fetchWestern);
+        }}
+      >
+        Western
+      </h3>
+      <h3
+        onClick={() => {
+          handleOption(moviesRequests.fetchAnimation);
+        }}
+      >
+        Animation
+      </h3>
+      <h3
+        onClick={() => {
+          handleOption(moviesRequests.fetchTv);
+        }}
+      >
+        Movie
+      </h3>
     </div>
   );
 };

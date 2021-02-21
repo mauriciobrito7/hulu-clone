cc705b8b0a2715f7739f6e3d59d6c8057ab3957b
Mauricio <maubot01@gmail.com>
2021-02-21 01:03:57 -0400

    requests of api

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,17 +1,17 @@
-const API_KEY = "d3b18a32c12a4ddb42ccdf68e8eef600";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
-const moviesRequest = {
+const moviesRequests = {
   fecthTrending: `/trending/all/week?api_key=${API_KEY}&language=en=US`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en=US`,
-  fecthActionMovies: `/discover/movie?api_key${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/movie?api_key${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/movie?api_key${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key${API_KEY}&with_genres=10749`,
-  fetchMystery: `/discover/movie?api_key${API_KEY}&with_genres=9648`,
-  fetchSciFi: `/discover/movie?api_key${API_KEY}&with_genres=878`,
-  fetchWestern: `/discover/movie?api_key${API_KEY}&with_genres=37`,
-  fetchAnimation: `/discover/movie?api_key${API_KEY}&with_genres=16`,
-  fetchTv: `/discover/movie?api_key${API_KEY}&with_genres=10770`,
+  fecthActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchMystery: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+  fetchSciFi: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+  fetchWestern: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+  fetchAnimation: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+  fetchTv: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
 };
 
-export default moviesRequest;
+export default moviesRequests;
